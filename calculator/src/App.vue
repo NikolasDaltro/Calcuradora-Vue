4e5ceb3b0080277c4f29fb154c556d0a2671f81d
NikolasDaltro <nikolasdsantos@gmail.com>
2022-11-18 10:58:46 -0300

    adicionando stilo

--- calculator/src/App.vue
+++ calculator/src/App.vue
@@ -11,5 +11,24 @@ export default {
 </script>
 
 <style>
-
+@font-face {
+    font-family:Arial, Helvetica, sans-serif
+}
+*{
+    font-family: Arial, Helvetica, sans-serif;
+}
+body{
+    margin: 0;
+}
+#app{
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    
+    color: white;
+    background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 40, 73));
+}
 </style>
